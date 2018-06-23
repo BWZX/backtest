3c0af8ca90f5126131dceba04af6ad4fd962fd00
adajass <jass.ada@qq.com>
2018-06-24 01:22:31 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,21 @@
+config = {
+  "base": {
+    "start_date": "2016-06-01",
+    "end_date": "2016-12-01",
+    "frequency": "1d",
+    "benchmark": "000001.XSHG",
+    "accounts": {
+        "stock": 1000000,
+        "future": 1000000,
+    }
+  },
+  "extra": {
+    "log_level": "verbose",
+  },
+  "mod": {
+    "sys_analyser": {
+      "enabled": True,
+      "plot": True
+    }
+  }
+}
