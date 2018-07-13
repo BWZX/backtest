18c0c3e03ec5d0442954e668d915f1aa98cadaf9
adajass <jass.ada@qq.com>
2018-07-13 20:17:51 +0800

    kdjg

--- btest.py
+++ btest.py
@@ -1,6 +1,6 @@
 # run_func_demo
 from rqalpha.api import *
-from rqalpha.core.strategy_context import StrategyContext
+from xmlrpc.client import ServerProxy
 from rqalpha import run_func
 import numpy as np
 from datetime import datetime
@@ -10,6 +10,7 @@ import tushare as ts
 import pandas as pd
 import os
 
+_RPC = client = ServerProxy("http://node0:8898")
 _INDUSTRY = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)),'industry.csv'))
 _ALL_INDUSTRY = ['电子器件', '玻璃行业', '商业百货', '传媒娱乐', '造纸行业', '生物制药', '陶瓷行业', '印刷包装', '建筑建材', '石油行业', '纺织机械', '环保行业', '飞机制造', '其它行业', '船舶制造', '物资外贸', '摩托车', '机械行业', '医疗器械', '家具行业', '公路桥梁', '钢铁行业', '水泥行业', '交通运输', '食品行业', '发电设备', '化工行业', '塑料制品', '仪器仪表', '有色金属', '金融行业', '供水供气', '煤炭行业', '农林牧渔', '酿酒行业', '房地产', '电器行业', '综合行业', '次新股', '汽车制造', '农药化肥', '酒店旅游', '家电行业', '电子信息', '化纤行业', '开发区', '电力行业', '纺织行业', '服装鞋类']
 
@@ -48,7 +49,7 @@ def code_to_id(code):
         code_id = code + '.XSHE'
     return code,code_id
 
-def Hs300IndustryRate(start_date, context):  #bug may apear here for the lack of industry.csv of some stock
+def Hs300IndustryRate(start_date, context, bar_dict):  #bug may apear here for the lack of industry.csv of some stock
     temdate = start_date.split('-')
     date = temdate[0]+temdate[1]
     dfM = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)),'hs300.csv'))
@@ -61,10 +62,6 @@ def Hs300IndustryRate(start_date, context):  #bug may apear here for the lack of
         if int(date) < int(d):
             break
 
-    # cursor = financetable.find({'code':'000001'}).sort('date')
-    # struct = equitystructure.find({'code': '000001'}).sort('report_date')
-    # print(list(cursor))
-    # print(list(struct))
     code_list = list(dfM[thedate])
     sto_basics = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)),'all.csv'))
     
@@ -73,23 +70,29 @@ def Hs300IndustryRate(start_date, context):  #bug may apear here for the lack of
     sto_basics = sto_basics.set_index('code')
     sto_outstand = sto_basics.to_dict(orient='dict')['outstanding']
     # import pdb;pdb.set_trace()
-    # print(code_list)
     industry_rate ={}
     hs300AllMarkets = 0.0 
     for code in code_list:
         code,code_id = code_to_id(code)
-        # print(code)
-        if sto_outstand.get(int(code)):
+        if sto_outstand.get(int(code)) and sto_outstand.get(int(code)) is not np.nan:
             outstand = sto_outstand.get(int(code))
         else:
             outstand = 1.0
-        price = history_bars(code_id, 1,'1d', 'close')[0]
-        # print(7777777777,' ',code_id,' ',price,)
+        
+        # import pdb; pdb.set_trace()
+        try:
+            price = bar_dict[code_id].close
+        except Exception:
+            price = 10.0     ####################
+
+        if price is np.nan:
+            price = 10.0 ############!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         market_value = outstand *1e8* price
         hs300AllMarkets += market_value
-        # print(888887888)
-        # import pdb;pdb.set_trace()  
-        ind = _INDUSTRY[_INDUSTRY.code == int(code)]['c_name'].values[0]
+        if len(_INDUSTRY[_INDUSTRY.code == int(code)]['c_name'].values) <=0:
+            ind = 'None'   #will repair later
+        else:
+            ind = _INDUSTRY[_INDUSTRY.code == int(code)]['c_name'].values[0]
         # print(ind,'----',market_value)
         if industry_rate.get(ind):
             # print('inside add ',industry_rate[ind])
@@ -101,32 +104,38 @@ def Hs300IndustryRate(start_date, context):  #bug may apear here for the lack of
     # print('loop all')
     for inds in industry_rate:
         industry_rate[inds] = industry_rate[inds]/ hs300AllMarkets
+    
     return industry_rate
 
 
 
 def init(context):
     context.future = 'IF88'
-    # context.s2 = "000001.XSHE"
     context.done=False
     subscribe(context.future)
-    # print(dir(context))
-    # import pdb;pdb.set_trace()   
-    # print(context.run_info.stock_starting_cash , ' ------------------------------')
-     
+    context.lastStocks=0
+    context.stocks=0
+    scheduler.run_monthly(before_trade, tradingday=1)  
     pass
 
-def before_trade(context):
+def before_trade(context, bar_dict):
     # test_uncomplete(datetime.strftime(context.now,'%Y-%m-%d'), context)
     # exit()
-    stocks = getTradeStocks() 
-    # print('ssssssss',datetime.strftime(context.now,'%Y-%m-%d'))   
-    industry_rate = Hs300IndustryRate(datetime.strftime(context.now,'%Y-%m-%d'), context)
+    context.dayDeal = False
+    print('now is: ',datetime.strftime(context.now,'%Y-%m-%d'))
+    stocks = _RPC.getTradeStocks(datetime.strftime(context.now,'%Y-%m-%d'),'m') 
+    if stocks.count('sh000300'):
+        stocks.remove('sh000300')    
+    stocks.sort()  #assert that it is list!!!
+    industry_rate = Hs300IndustryRate(datetime.strftime(context.now,'%Y-%m-%d'), context, bar_dict)
     context.sto_moneyall = context.run_info.stock_starting_cash
     industry_count={}
-    # industry_
     for sto in stocks:
-        ind = _INDUSTRY[_INDUSTRY.code == int(sto)]['c_name'].values[0]
+        if len(_INDUSTRY[_INDUSTRY.code == int(sto)]['c_name'].values) <=0:
+            ind = 'None'   #will repair later
+        else:
+            ind = _INDUSTRY[_INDUSTRY.code == int(sto)]['c_name'].values[0]
+
         if industry_count.get(ind):
             industry_count[ind] += 1
         else:
@@ -134,9 +143,15 @@ def before_trade(context):
         
     context.industry_count = industry_count #amount of certain industry
     context.industry_rate = industry_rate  #market value of industry among all
+    context.lastStocks = context.stocks
     context.stocks = stocks  # the deal stock list or dict
 
-def deal_stocks(context, bar_dict):    
+
+def deal_stocks(context, bar_dict):  
+    if context.portfolio.stock_account.market_value > 10:        
+        for sto in context.portfolio.stock_account.positions:
+            order_target_value(sto,0)
+
     if type(context.stocks) == type({}):
         money = 0 
         for sto in context.stocks:
@@ -150,35 +165,48 @@ def deal_stocks(context, bar_dict):
         money = 0 
         for sto in context.stocks:
             code, code_id = code_to_id(sto)
-            ind = _INDUSTRY[_INDUSTRY.code == int(code)]['c_name'].values[0]
-            # print('iam here,')
-            # import pdb;pdb.set_trace()  
-            print('the count is： ',context.industry_count[ind],' the code ',code, ' rate: ',context.industry_rate.get(ind))
-            money = context.industry_rate[ind] * context.sto_moneyall / context.industry_count[ind]
-            print('before deal ', money)
-            #"deal the stocks"
+            if len(_INDUSTRY[_INDUSTRY.code == int(sto)]['c_name'].values) <=0:
+                ind = 'None'   #will repair later
+            else:
+                ind = _INDUSTRY[_INDUSTRY.code == int(sto)]['c_name'].values[0]
+            if not context.industry_rate.get(ind) and context.industry_count.get(ind):
+                money = context.sto_moneyall / len(context.stocks)
+            else:
+                money = context.industry_rate[ind] * context.sto_moneyall / context.industry_count[ind]                
+            money = context.sto_moneyall / len(context.stocks)
+            # import pdb;pdb.set_trace()
+            if context.portfolio.stock_account.cash < money:
+                money = context.portfolio.stock_account.cash * 0.88
+                pass
             order_value(code_id,money)
-            print('deal ',code_id,' with money of ',money)
+            # print('deal ',code_id,' with money of ',money)
 
 
 def deal_future(context, bar_dict):
     sto_market_value = context.portfolio.stock_account.market_value
-    if sto_market_value <100:
+    if sto_market_value < 100:
         return
+    if context.portfolio.future_account.market_value < -100:
+        buy_close(context.future, context.futureLots)
     price = bar_dict[context.future].close
-    lots = int(sto_market_value/price)
+    print("sto value: ", sto_market_value,' IF88 price: ', price)
+    lots = int(sto_market_value/300/price)
+    context.futureLots = lots
+    # lots = 1
     sell_open(context.future, lots)  
+    # import pdb;pdb.set_trace()
+    print('deal future: ',lots)
     pass
 
 def handle_bar(context, bar_dict):    
-    if not context.done:
-        before_trade(context)
+    if not context.stocks:
+        return
+    
+    if context.stocks != context.lastStocks:
         deal_stocks(context, bar_dict)
-        # deal_future(context, bar_dict)
-        # order_percent('000001.XSHE', 0.9)
-        # sell_open(context.future, 1)        
-        context.done=True
-    # import pdb;pdb.set_trace()
+        deal_future(context, bar_dict)
+        context.lastStocks = context.stocks
+    
     pass
     
 run_func(init=init, handle_bar=handle_bar, config=config)
--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 config = {
   "base": {
-    "start_date": "2016-05-01",
-    "end_date": "2016-12-01",
+    "start_date": "2013-01-01",
+    "end_date": "2017-12-01",
     "frequency": "1d",
     "margin_multiplier": 1,
     "benchmark": "000001.XSHG",
     "accounts": {
-        "stock": 1000000,
-        "future": 1000000,
+        "stock":  50000000,
+        "future": 60000000,
     }
   },
   "extra": {
